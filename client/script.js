655b42b90da571e516cdc42699c3121294397e5a
My namebram@theosmans.com <bram@upstager.co>
2023-04-23 23:35:29 -0400

    update the server

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
 
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
-    const response = await fetch('https://gpt-connect.onrender.com/', {
+    const response = await fetch('https://gpt-final-delta.vercel.app/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
